c18c6fa4a5fd4f04d85747ffaa846964e87e5e2a
Mikel Bober-Irizar <mikel@mxbi.net>
2016-05-31 10:24:46 +0100

    Modify package names

--- python-package/setup.py
+++ python-package/setup.py
@@ -20,10 +20,10 @@ LIB_PATH = libpath['find_lib_path']()
 print("Install libxgboost from: %s" % LIB_PATH)
 # Please use setup_pip.py for generating and deploying pip installation
 # detailed instruction in setup_pip.py
-setup(name='xgboost',
+setup(name='minixgboost',
       version=open(os.path.join(CURRENT_DIR, 'xgboost/VERSION')).read().strip(),
       # version='0.4a23',
-      description="XGBoost Python Package",
+      description="MiniXGBoost Python Package",
       long_description=open(os.path.join(CURRENT_DIR, 'README.rst')).read(),
       install_requires=[
           'numpy',
@@ -37,4 +37,4 @@ setup(name='xgboost',
       # this is the golden line
       include_package_data=True,
       data_files=[('xgboost', LIB_PATH)],
-      url='https://github.com/dmlc/xgboost')
+      url='https://github.com/mxbi/minixgboost')
